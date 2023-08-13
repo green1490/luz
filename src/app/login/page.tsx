480665a54c67e35a3a2932abe13b99b404f5de1c
green1490 <zambokrisztian1490@gmail.com>
2023-08-13 15:38:32 +0200

    can login and change bg color

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,66 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Login() {
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+    const login = async () => {
+        await fetch('http://localhost:5246/login/?' + new URLSearchParams({
+            Email:email,
+            Password: password
+        }), {
+            method:'GET',
+            mode:'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+    }
+    
     return (
         <>
-            <div className="h-screen grid place-items-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" >
-                <p className="font-['Times_New_Roman'] text-7xl">Luz</p>
-                <input autoComplete="off" className="px-2 outline outline-1 bg-transparent h-10" placeholder="Email" type="email" name="email"></input>
-                <div className="text-center">
-                    <input autoComplete="off" className="px-2 outline outline-1 bg-transparent h-10" placeholder="Password" type="password" name="password"></input>
-                    <p className="row-start-4">Dont have an account? <a className="text-cyan-700/80" href="/registration">Sign Up</a></p>
+            <div 
+                className="h-screen grid place-items-center bg-gradient-to-r from-slate-500 to-green-500" >
+                <p 
+                    className="font-['Times_New_Roman'] text-7xl">
+                    Luz
+                </p>
+                <input 
+                    autoComplete="off" 
+                    className="px-2 outline outline-1 bg-transparent h-10" 
+                    placeholder="Email" 
+                    type="email" 
+                    value={email} 
+                    onChange={e => setEmail(e.target.value)} 
+                    name="email">
+                </input>
+                <div 
+                    className="text-center">
+                    <input 
+                        autoComplete="off" 
+                        className="px-2 outline outline-1 bg-transparent h-10" 
+                        placeholder="Password" 
+                        type="password" 
+                        value={password} 
+                        onChange={p => setPassword(p.target.value)} 
+                        name="password">
+                    </input>
+                    <p 
+                        className="row-start-4">
+                        Dont have an account? 
+                        <a className="pl-2 text-blue-700/80" href="/registration">
+                            Sign Up
+                        </a>
+                    </p>
                 </div>
-                <button className="row-start-6 w-64 outline outline-offset-2 outline-1 text-xl">Log in</button>
+                <button 
+                    onClick={login} 
+                    className="row-start-6 w-64 outline outline-offset-2 outline-1 text-xl">
+                        Log in
+                </button>
             </div>
         </>
     )
