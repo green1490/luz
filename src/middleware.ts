7ade7f37032036d64444d4417e3702891fefdb90
green1490 <zambokrisztian1490@gmail.com>
2023-08-15 14:07:18 +0200

    need to sign in

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,16 @@ export function middleware(request: NextRequest) {
   if (request.cookies.has("userData") == false) {
     const { pathname } = request.nextUrl
     
-    if (pathname != '/login') {
-        return NextResponse.rewrite(new URL('/login', request.url))
+    if (!['/login','/registration'].includes(pathname)) {
+      return NextResponse.redirect(new URL('/login', request.url))
+    } else {
+      return NextResponse.next()
     }
-  }
+   }
 }
 
 export const config = {
   matcher: [
-    '/', '/login', '/registration'
+    '/', '/login', '/registration',
   ],
 }
